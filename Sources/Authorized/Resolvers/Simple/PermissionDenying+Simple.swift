e5138d64a81ba2ccce48cd0bc25ab6da3cc6e491
Zdenek Topic <zdenek@zdnkt.com>
2018-10-29 17:53:50 +0100

    Simple deny

---
+++ Sources/Authorized/Resolvers/Simple/PermissionDenying+Simple.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+extension PermissionDenying {
+    
+    public func deny<R, A>(_ resource: R.Type, _ action: R.Action, for user: A.Type) where R: Resource, A: Authorizable {
+        let request = PermissionRequest(
+            authorizable: A.authorizableIdentifier,
+            resource: R.resourceIdentifier,
+            action: action.actionIdentifier,
+            isInstance: false
+        )
+        
+        deny(
+            with: request,
+            resolver: SimplePermissionResolver(value: true)
+        )
+    }
+    
+}
