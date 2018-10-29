b18aa8e22532302ed35934232507ed8c24b97ff6
Zdenek Topic <zdenek@zdnkt.com>
2018-10-29 17:50:37 +0100

    Request uses PermissionVerifying.authorize in Request.authorize

--- Sources/Authorized/Permissions.swift
+++ Sources/Authorized/Permissions.swift
@@ -29,6 +29,12 @@ extension PermissionVerifying {
         }
     }
     
+    public func authorize<R, A>(_ resource: R.Type, _ action: R.Action, as user: A) throws where R: Resource, A: Authorizable{
+        guard allowed(R.self, action, as: user) else {
+            throw Abort(.forbidden)
+        }
+    }
+    
     public func allowed<R, A>(_ resource: R, _ action: R.Action, as user: A) -> Bool where R: Resource, A: Authorizable {
         return allowed(
             .instance(resource),
--- Sources/Authorized/Request+Authorize.swift
+++ Sources/Authorized/Request+Authorize.swift
@@ -5,36 +5,28 @@ extension Request {
     
     public func authorize<A, R>(_: A.Type, _ resource: R, _ action: R.Action) throws where A: Authorizable & Authenticatable, R: Resource {
         let user = try self.requireAuthenticated(A.self)
-        let permissions = try self.make(PermissionManager.self)
+        let permissions = try self.make(Permissions.self)
         
-        guard permissions.allowed(resource, action, as: user) else {
-            throw Abort(.forbidden)
-        }
+        try permissions.authorize(resource, action, as: user)
     }
     
     public func authorize<A, R>(_ user: A, _ resource: R, _ action: R.Action) throws where A: Authorizable, R: Resource {
-        let permissions = try self.make(PermissionManager.self)
+        let permissions = try self.make(Permissions.self)
         
-        guard permissions.allowed(resource, action, as: user) else {
-            throw Abort(.forbidden)
-        }
+        try permissions.authorize(resource, action, as: user)
     }
     
     public func authorize<A, R>(_: A.Type, _ resource: R.Type, _ action: R.Action) throws where A: Authorizable & Authenticatable, R: Resource {
         let user = try self.requireAuthenticated(A.self)
-        let permissions = try self.make(PermissionManager.self)
+        let permissions = try self.make(Permissions.self)
         
-        guard permissions.allowed(resource, action, as: user) else {
-            throw Abort(.forbidden)
-        }
+        try permissions.authorize(resource, action, as: user)
     }
     
     public func authorize<A, R>(_ user: A, _ resource: R.Type, _ action: R.Action) throws where A: Authorizable, R: Resource {
-        let permissions = try self.make(PermissionManager.self)
+        let permissions = try self.make(Permissions.self)
         
-        guard permissions.allowed(resource, action, as: user) else {
-            throw Abort(.forbidden)
-        }
+        try permissions.authorize(resource, action, as: user)
     }
     
 }
