11b013a6698dd842e20c6a4a3bc46f498584d18c
Zdenek Topic <zdenek@zdnkt.com>
2018-11-02 00:16:27 +0100

    ResourcePolicyDefinition configuration

--- Sources/Authorized/Policies/ResourcePolicyDefinition.swift
+++ Sources/Authorized/Policies/ResourcePolicyDefinition.swift
@@ -2,22 +2,52 @@ import Foundation
 
 public struct ResourcePolicyDefinition<R: Resource> {
     
-    var map: [String: [String: PermissionResolving]] = [:]
-    
-    public mutating func action<A>(_ action: R.Action, to: (R, A) -> PermissionResolution) where A: Authorizable {
-        
+    public enum Error: Swift.Error {
+        case alreadyExists(action: R.Action, authorizable: Authorizable.Type)
     }
     
-    public mutating func action<A>(_ action: R.Action, to: (A) -> PermissionResolution) where A: Authorizable {
+    var map: [String: [String: (PermissionRequest, PermissionResolving)]] = [:]
+    
+    public init() {}
+    
+    public mutating func action<A>(_ action: R.Action, to: @escaping (R, A) -> PermissionResolution) throws where A: Authorizable {
+        let resolver = InstanceClosurePermissionResolver(to)
         
+        try add(action: action, as: A.self, instance: true, resolver: resolver)
     }
     
-    private mutating func add<A>(action: R.Action, as: A.Type, resolver: PermissionResolving) {
+    public mutating func action<A>(_ action: R.Action, to: @escaping (A) -> PermissionResolution) throws where A: Authorizable {
+//        let resolver = InstanceClosurePermissionResolver(to)
+//
+//        try add(action: action, as: A.self, instance: true, resolver: resolver)
+    }
+    
+    private mutating func add<A>(action: R.Action, as user: A.Type, instance: Bool, resolver: PermissionResolving) throws where A: Authorizable {
+        if map[action.actionIdentifier] == nil {
+            map[action.actionIdentifier] = [:]
+        }
         
+        if map[action.actionIdentifier]?[A.authorizableIdentifier] != nil {
+            throw Error.alreadyExists(action: action, authorizable: A.self)
+        }
+        
+        let request = PermissionRequest(
+            authorizable: A.self,
+            resource: R.self,
+            action: action,
+            isInstance: instance
+        )
+        
+        map[action.actionIdentifier]?[A.authorizableIdentifier] = (request, resolver)
     }
     
     internal func add(to config: PermissionDefining) {
-        
+        map.values
+            .forEach {
+                $0.forEach {
+                    config.define(with: $0.value.0, resolver: $0.value.1)
+                }
+            }
     }
     
 }
