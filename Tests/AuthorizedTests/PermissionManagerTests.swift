bf7e52671e34c69ac652826f4b485908c4eda4cf
Zdenek Topic <zdenek@zdnkt.com>
2018-11-06 23:51:26 +0100

    Add tests for before

--- Tests/AuthorizedTests/PermissionManagerTests.swift
+++ Tests/AuthorizedTests/PermissionManagerTests.swift
@@ -9,6 +9,7 @@ final class PermissionManagerTests: XCTestCase {
     static var allTests = [
         ("testAllowed", testAllowed),
         ("testAuthorize", testAuthorize),
+        ("testBefore", testBefore),
     ]
     
     func testAllowed() throws {
@@ -77,11 +78,48 @@ final class PermissionManagerTests: XCTestCase {
         )
     }
     
+    func testBefore() throws {
+        let container = try self.container()
+        let permissions = try container.make(PermissionManager.self)
+        
+        let admin = SomeUser(id: 1, admin: true)
+        let author = SomeUser(id: 2)
+        let hacker = SomeUser(id: 3)
+        let post = Post(id: 1, userId: author.id)
+        
+        permissions.before { (_, _, anyUser, container) -> EventLoopFuture<PermissionResolution?> in
+            guard let user = anyUser as? SomeUser else {
+                return container.future(nil)
+            }
+            
+            if user.isAdmin {
+                // Admins can do anything!
+                return container.future(.allow)
+            }
+            
+            return container.future(nil)
+        }
+        
+        permissions.allow(Post.self, .modify, as: SomeUser.self) { post, user, container in
+            return container.future(post.userId == user.id)
+        }
+        
+        XCTAssertTrue(try permissions.allowed(post, .modify, as: admin, on: container).wait())
+        XCTAssertTrue(try permissions.allowed(post, .modify, as: author, on: container).wait())
+        XCTAssertFalse(try permissions.allowed(post, .modify, as: hacker, on: container).wait())
+    }
+    
 }
 
 fileprivate struct SomeUser: Authorizable, Authenticatable {
     
     let id: Int
+    let isAdmin: Bool
+    
+    init(id: Int, admin: Bool = false) {
+        self.id = id
+        self.isAdmin = admin
+    }
     
 }
 
