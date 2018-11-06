7cba69156fd171adf5a91c07a2b33f3b8699ebae
Zdenek Topic <zdenek@zdnkt.com>
2018-11-06 16:19:54 +0100

    Make extensions on Authorizable publis

--- Sources/Authorized/Authorizable.swift
+++ Sources/Authorized/Authorizable.swift
@@ -12,12 +12,12 @@ extension Authorizable {
         return String(describing: Self.self)
     }
     
-    func can<R>(_: R.Type, _ action: R.Action, on container: Container) throws -> Future<Bool> where R: Resource {
+    public func can<R>(_: R.Type, _ action: R.Action, on container: Container) throws -> Future<Bool> where R: Resource {
         let permissions = try container.make(PermissionManager.self)
         return permissions.allowed(R.self, action, as: self, on: container)
     }
     
-    func authorize<R>(_: R.Type, _ action: R.Action, on container: Container) throws -> Future<Void> where R: Resource {
+    public func authorize<R>(_: R.Type, _ action: R.Action, on container: Container) throws -> Future<Void> where R: Resource {
         return try can(R.self, action, on: container)
             .map { result in
                 guard result else {
@@ -26,12 +26,12 @@ extension Authorizable {
             }
     }
     
-    func can<R>(_ resource: R, _ action: R.Action, on container: Container) throws -> Future<Bool> where R: Resource {
+    public func can<R>(_ resource: R, _ action: R.Action, on container: Container) throws -> Future<Bool> where R: Resource {
         let permissions = try container.make(PermissionManager.self)
         return permissions.allowed(resource, action, as: self, on: container)
     }
     
-    func authorize<R>(_ resource: R, _ action: R.Action, on container: Container) throws -> Future<R> where R: Resource {
+    public func authorize<R>(_ resource: R, _ action: R.Action, on container: Container) throws -> Future<R> where R: Resource {
         return try can(resource, action, on: container)
             .map { result in
                 guard result else {
