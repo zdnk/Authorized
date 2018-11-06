f9acb40a61efd944583b364977f3019a8ecd241a
Zdenek Topic <zdenek@zdnkt.com>
2018-11-06 23:31:24 +0100

    Before closures beign called

--- Sources/Authorized/Manager/PermissionManager.swift
+++ Sources/Authorized/Manager/PermissionManager.swift
@@ -16,21 +16,53 @@ open class PermissionManager: PermissionVerifying {
     }
     
     open func allowed<R, A>(_ target: ResourceTarget<R>, _ action: R.Action, as user: A, on container: Container) -> Future<Bool> where R : Resource, A : Authorizable {
-        let permissions = self.permissions(
-            action: action.actionIdentifier,
-            resource: R.resourceIdentifier,
-            user: user,
-            instance: target.isInstance
-        )
         
-        return self.resolve(
-            permissions,
-            target: target,
-            user: user,
-            on: container
-        ).map { resolution -> Bool in
-            return resolution.isAllow
+        var beforeFutures: [Future<PermissionResolution?>] = []
+        
+        do {
+            try beforeClosures.forEach { closure in
+                let future = try closure(target, action, user, container)
+                beforeFutures.append(future)
+            }
+        } catch {
+            return container.future(false)
         }
+        
+        return beforeFutures
+            .flatten(on: container)
+            .map { results -> PermissionResolution? in
+                if results.contains(.deny) {
+                    return .deny
+                }
+                
+                if results.contains(.allow) {
+                    return .allow
+                }
+                
+                return nil
+            }
+            .flatMap { result -> Future<PermissionResolution> in
+                if let result = result {
+                    return container.future(result)
+                }
+                
+                let permissions = self.permissions(
+                    action: action.actionIdentifier,
+                    resource: R.resourceIdentifier,
+                    user: user,
+                    instance: target.isInstance
+                )
+                
+                return self.resolve(
+                    permissions,
+                    target: target,
+                    user: user,
+                    on: container
+                )
+            }
+            .map { resolution -> Bool in
+                return resolution.isAllow
+            }
     }
     
 }
